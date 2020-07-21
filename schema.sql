b73c534e2864c3fff447b3e0989c1bd363f9fafe
Ayla Dillis <aayladillis@gmail.com>
2020-07-21 09:52:51 -0700

    fix bugs

--- schema.sql
+++ schema.sql
@@ -36,9 +36,9 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Kelechi', 'Okoro', 12, 100); 
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ('Marketing Manager', 50043.00, 420);
+VALUES ('Marketing Manager', 50043.00, 40);
 
 INSERT INTO department (employeeName)
-VALUES ('Ayla');
+VALUES ('Nick');
 
 
