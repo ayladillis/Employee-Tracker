0011de6ca911083f602629abdd83fb3a79af2531
Ayla Dillis <aayladillis@gmail.com>
2020-07-20 18:35:09 -0700

    dependencies

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ USE employee_trackerDB;
 
 CREATE TABLE employee (
   -- Code Starts Here
-id INT NOT NULL, 
+id INT NOT NULL AUTO_INCREMENT, 
 first_name VARCHAR(100),
 last_name VARCHAR(100),
 role_id INT(10),
@@ -16,7 +16,7 @@ PRIMARY KEY (id)
 
 CREATE TABLE roles (
   -- Code Starts Here
-id INT NOT NULL, 
+id INT NOT NULL AUTO_INCREMENT, 
 title VARCHAR(10),
 salary DECIMAL(10,2),
 department_id INT(10),
@@ -26,14 +26,19 @@ PRIMARY KEY (id)
 
 CREATE TABLE department (
   -- Code Starts Here
-id INT NOT NULL, 
+id INT NOT NULL AUTO_INCREMENT, 
 employeeName VARCHAR(10),
 PRIMARY KEY (id)
   -- Code Ends Here
 );
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-values ('Kelechi', 'Okoro', 12, 100); 
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+-- VALUES ('Kelechi', 'Okoro', 12, 100); 
 
-INSERT roles (title, salary, department_id)
-values ('Marketing Manager', 500,000.00, 420)
+-- INSERT INTO roles (title, salary, department_id)
+-- VALUES ('Marketing Manager', 500,000.00, 420)
+
+-- INSERT INTO department (employeeName)
+-- VALUES ('Ayla')
+
+
