b90dccd8fe8d32b981d87206480cf6eafc75794c
Ayla Dillis <aayladillis@gmail.com>
2020-07-20 19:38:30 -0700

    setting up inquirer

--- employeetracker.js
+++ employeetracker.js
@@ -1,16 +1,12 @@
+// dependencies
 var mysql = require("mysql");
-const inquirer = require("inquirer");
+var inquirer = require("inquirer");
+var cTable = require("console.table")
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "password",
   database: "greatbayDB"
 });
@@ -18,11 +14,66 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    connection.end();
+    // connection.end();
   });
   
 inquirer.prompt ([
     {
-        
+        type: "list",
+        name: "first",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Employees By Department",
+            "View All Employees By Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager"
+        ]
+    },
+    {
+        type: "list",
+        name: "remove",
+        message: "Who would you like to remove?",
+        choices: [
+            " "
+        ]
+    },
+    {
+        type: "input",
+        name: "firstName",
+        message: "What is the employees first name?"
+    },
+    {
+        type: "input",
+        name: "lastName",
+        message: "What is the employees last name?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is the employees role?",
+        choices: [
+            "Sales Lead",
+            "Salesperson",
+            "Lead Engineer",
+            "Software Engineer",
+            "Account Manager",
+            "Accountant",
+            "Legal Team Lead"
+        ]
+    },
+    {
+        type: "list",
+        name: "manager",
+        message: "Who is the employees manager?",
+        choices: [
+            "None",
+            "Ayla Dillis",
+            "Sabrina Sulliman",
+            "Carolyn Hernandez",
+            "Alisa Nguyne-Le"
+        ]
     }
 ])  
--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS employee_trackerDB;
-CREATE database employee_trackerDB;
+DROP DATABASE IF EXISTS employeetrackerDB;
+CREATE database employeetrackerDB;
 
-USE employee_trackerDB;
+USE employeetrackerDB;
 
 CREATE TABLE employee (
   -- Code Starts Here
@@ -17,7 +17,7 @@ PRIMARY KEY (id)
 CREATE TABLE roles (
   -- Code Starts Here
 id INT NOT NULL AUTO_INCREMENT, 
-title VARCHAR(10),
+title VARCHAR(30),
 salary DECIMAL(10,2),
 department_id INT(10),
 PRIMARY KEY (id)
@@ -32,13 +32,13 @@ PRIMARY KEY (id)
   -- Code Ends Here
 );
 
--- INSERT INTO employee (first_name, last_name, role_id, manager_id) 
--- VALUES ('Kelechi', 'Okoro', 12, 100); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES ('Kelechi', 'Okoro', 12, 100); 
 
--- INSERT INTO roles (title, salary, department_id)
--- VALUES ('Marketing Manager', 500,000.00, 420)
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Marketing Manager', 50043.00, 420);
 
--- INSERT INTO department (employeeName)
--- VALUES ('Ayla')
+INSERT INTO department (employeeName)
+VALUES ('Ayla');
 
 
